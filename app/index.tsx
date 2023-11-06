0f2a3f9f4997dcc247e59c38e36b6adc77588307
Javel Wilson <javelawilson@gmail.com>
2023-11-05 23:28:55 -0500

    Add basic document scanner implementation

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
+import React, {useState} from 'react';
+import {Button, Image, StyleSheet, Text} from 'react-native';
+import DocumentScanner from 'react-native-document-scanner-plugin';
+import {FlatList} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-export default function Page() {
+const Page = () => {
+  const [scannedImage, setScannedImage] = useState<string[]>([]);
+
+  const scanDocument = async () => {
+    const {scannedImages} = await DocumentScanner.scanDocument();
+
+    if (scannedImages && scannedImages.length > 0) {
+      setScannedImage((s) => [...s, ...scannedImages]);
+    }
+
+    console.log('scannedImages', scannedImages);
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Scan Document" onPress={scanDocument} />
+      <FlatList
+        data={scannedImage}
+        renderItem={({item}) => (
+          <Image
+            source={{uri: item}}
+            style={{
+              width: '100%',
+              height: 300,
+              backgroundColor: '#000',
+            }}
+            resizeMode="contain"
+          />
+        )}
+        keyExtractor={(item) => item}
+      />
+    </SafeAreaView>
   );
-}
+};
+
+export default Page;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#fff',
     flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
   },
 });
